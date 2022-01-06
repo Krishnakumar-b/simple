25a7488e533f5c7c7c725e27ec5e5c33f6f41711
Krishna Kumar B <krishna.b@melento.com>
2022-01-06 15:24:28 +0530

    ched some code

--- program.go
+++ program.go
@@ -1,7 +1,28 @@
 package main
 
-import("fmt")
-func main(){
-fmt.Println("Hello")
+import (
+	"fmt"
+)
+
+func calculate(n int) int {
+
+	var sum, rem int
+	for n != 0 {
+		rem = n % 10
+		sum = sum + rem
+		n = n / 10
+	}
+
+	return sum
 }
-;l
+
+func main() {
+	number := calculate(9999)
+	for {
+		if number > 9 {
+			number = calculate(number)
+		}
+		break
+	}
+	fmt.Println(number)
+}
