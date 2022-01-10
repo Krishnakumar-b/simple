1a5b27735fbfd7fb2de49fbd2316830c15c0cf1d
Krishna Kumar B <krishna.b@melento.com>
2022-01-10 11:56:57 +0530

    new changes

--- program.go
+++ program.go
@@ -4,20 +4,37 @@ import (
 	"fmt"
 )
 
-func calculate(n int) int {
+func calculate(n int64) int64 {
+	var arrNum [3]int
+	var sum, rem int64
 
-	var sum, rem int
+	i := 0
+
 	for n != 0 {
 		rem = n % 10
+		if rem == 0 {
+			fmt.Print("error")
+		}
+		arrNum[i] = int(rem)
+		i++
 		sum = sum + rem
 		n = n / 10
 	}
+	// var mapNumber map[int]int
+	// mapNumber=arrNum
+	// for i=len(arrNum);i>=0;i-- {
 
+	// }
 	return sum
 }
 
 func main() {
-	n := 9999999999999
+	var n int64
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		fmt.Print(err)
+	}
+
 	number := calculate(n)
 	for {
 		if number > 9 {
@@ -25,5 +42,7 @@ func main() {
 		}
 		break
 	}
+
 	fmt.Println(n, number)
+
 }
