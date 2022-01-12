0cfe01f58e2311d14873651fb4cc9efb287c60fb
Krishna Kumar B <krishna.b@melento.com>
2022-01-12 13:44:58 +0530

    program using interface

--- optimization.interface.go
+++ optimization.interface.go
@@ -1,30 +1,49 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"go/types"
-// )
+import (
+	"fmt"
+)
 
-// type DataType interface {
-// }
+type DataType interface {
+}
 
-// func calculat(T DataType) error {
+func calculat(T DataType) error {
 
-// 	fmt.Println(T)
-// 	xtype := fmt.Sprintf("%T", T)
-// 	fmt.Println("*****", xtype)
+	//fmt.Println(T)
+	xtype := fmt.Sprintf("%T", T)
+	//fmt.Println("*****", xtype)
+	if xtype == "int" {
 
-// 	if xtype == types.Int {
-// 		fmt.Println("string")
-// 	}
-// 	return nil
-// }
-// func main() {
-// 	var i interface{}
+		n := T.(int)
 
-// 	i = 42
-// 	calculat(i)
+		for n/10 != 0 {
+			n = n%10 + n/10
+		}
+		fmt.Printf(" Type : %T\n", n)
+		fmt.Println("Number : ", n)
+		fmt.Println("Sum : ", n)
 
-// 	// i = "hello"
-// 	// calculat(i)
-// }
+	}
+	if xtype == "string" {
+		fmt.Println("Error!Cannot use string values")
+
+	}
+
+	if xtype == "float64" {
+		fmt.Println("Error!Cannot use float values")
+	}
+
+	return nil
+}
+func main() {
+	var i DataType
+
+	i = 9999
+	calculat(i)
+
+	i = "hello"
+	calculat(i)
+
+	i = 11111.10
+	calculat(i)
+}
